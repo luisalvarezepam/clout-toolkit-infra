ab17a08aba43a8dd6053e12b71b47f6787b3f914
Luis Alvarez <the_lg@hotmail.com>
2025-06-20 15:46:57 -0600

    fix network

--- infra/modules/network/main.tf
+++ infra/modules/network/main.tf
@@ -2,7 +2,7 @@ resource "azurerm_virtual_network" "vnet" {
   name                = "vnet-${var.name_suffix}"
   location            = var.location
   resource_group_name = var.resource_group_name
-  address_space       = ["10.0.0.0/16"]  # Corregir de /8 a /16
+  address_space       = ["10.0.0.0/8"]
 
   tags = var.tags
 }
@@ -11,21 +11,21 @@ resource "azurerm_subnet" "public" {
   name                 = "subnet-public"
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]  # Corregir para estar dentro de 10.0.0.0/16
+  address_prefixes     = ["10.4.0.0/26"]
 }
 
 resource "azurerm_subnet" "private" {
   name                 = "subnet-private"
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.2.0/24"]  # Corregir para estar dentro de 10.0.0.0/16
+  address_prefixes     = ["10.3.0.0/16"]
 }
 
 resource "azurerm_subnet" "db" {
   name                 = "subnet-db"
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.3.0/24"]  # Corregir para estar dentro de 10.0.0.0/16
+  address_prefixes     = ["10.2.0.0/26"]
   delegation {
     name = "delegation"
 
